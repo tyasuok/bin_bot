06864386343eb29680673ecb45571859b4b94ffb
Thomas Yasuoka <thomas.yasuoka@outlook.com>
2022-07-02 08:26:11 -0300

    allow for different pairs

--- src/websockets.rs
+++ src/websockets.rs
@@ -5,8 +5,8 @@ use futures_util::{future, StreamExt};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use std;
 
-pub async fn test() -> Result<(), Box<dyn std::error::Error>> {
-    let (ws_stream, _) = connect_async("wss://stream.binance.com:9443/ws/bnbbtc@bookTicker").await?;
+pub async fn test(a: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let (ws_stream, _) = connect_async(format!("wss://stream.binance.com:9443/ws/{}", a)).await?;
     let (write, mut read) = ws_stream.split();
     loop {
         let message = read.next().await.unwrap()?;
@@ -22,7 +22,8 @@ pub async fn test() -> Result<(), Box<dyn std::error::Error>> {
             Message::Frame(_) => {println!("Frame")}
             Message::Close(e) => {println!("{:?}", e)}
         }
-        tokio::time::sleep(core::time::Duration::from_secs(5));
+        // std::thread::sleep(std::time::Duration::from_secs(5));
+        // tokio::time::sleep(core::time::Duration::from_secs(5));
     }
     Ok(())
 }
