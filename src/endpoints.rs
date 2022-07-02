3cad5bb28c97592737cedbb0e726b8e0009b7d28
Thomas Yasuoka <thomas.yasuoka@outlook.com>
2022-07-02 08:26:41 -0300

    trying out testnet

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -2,9 +2,13 @@
 // mod json_formats;
 use crate::json_formats;
 use std::error;
+use std::env;
+use sha2::Sha256;
+use hmac::{Hmac, Mac};
 
 pub async fn kline(symbol: &str) -> Result<Vec<Vec<json_formats::Kline>>, Box<dyn error::Error>> {
-    let response = reqwest::get(format!("https://api.binance.com/api/v3/klines?symbol={}&interval=1h", {symbol}))
+    // let response = reqwest::get(format!("https://api.binance.com/api/v3/klines?symbol={}&interval=1h", {symbol}))
+    let response = reqwest::get(format!("https://testnet.binance.vision/api/v3/klines?symbol={}&interval=1h", {symbol}))
         .await?
         .json::<Vec<Vec<json_formats::Kline>>>()
         .await?;
@@ -12,9 +16,30 @@ pub async fn kline(symbol: &str) -> Result<Vec<Vec<json_formats::Kline>>, Box<dy
 }
 
 pub async fn exc_info() -> Result<json_formats::APIResponse, Box<dyn error::Error>> {
-    let response = reqwest::get("https://www.binance.com/api/v3/exchangeInfo")
+    // let response = reqwest::get("https://www.binance.com/api/v3/exchangeInfo")
+    let response = reqwest::get("https://testnet.binance.vision/api/v3/exchangeInfo")
         .await?
         .json::<json_formats::APIResponse>()
         .await?;
     Ok(response)
 }
+
+// pub async fn sub_assets() -> Result<json_formats::APIResponse, Box<dyn error::Error>> {
+pub async fn sub_assets() -> Result<std::string::String, Box<dyn error::Error>> {
+    // let response = reqwest::get("https://www.binance.com/api/v3/exchangeInfo")
+    let response = reqwest::get("https://testnet.binance.vision/sapi/v3/sub-account/assets")
+        .await?
+        .text()
+        // .json::<json_formats::APIResponse>()
+        .await?;
+    Ok(response)
+}
+
+// pub async fn test() -> Result<std::string::String, Box<dyn error::Error>> {
+//     type ko = Hmac<Sha256>;
+//     let mut t = ko::new_from_slice(b"my secret and secure key").await?;
+//     t.update(b"input message");
+//     let result = t.finalize();
+//     Ok(t.into_bytes())
+// 
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
 use bin_bot::endpoints;
 use bin_bot::websockets;
+use sha2::Sha256;
+use hmac::{Hmac, Mac};
+use hex_literal::hex;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    websockets::test().await?;
-
-    Ok(())
+// #[tokio::main]
+// async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//     // websockets::test("btcusdt@bookTicker").await?;
+//     // println!("{:?}", endpoints::kline("BTCUSDT").await?);
+//     // println!("{:?}", endpoints::sub_assets().await?);
+//     // println!("{:?}", endpoints::test().await?);
+//     //
+//     type ko = Hmac<Sha256>;
+//     let mut t = ko::new_from_slice(b"my secret and secure key").await?;
+//     t.update(b"input message");
+//     let result = t.finalize();
+// 
+//     Ok(())
+// }
+//
+fn main() {
+    type ko = Hmac<Sha256>;
+    let mut t = ko::new_from_slice(b"my secret and secure key").expect("huh");
+    t.update(b"input message");
+    let result = t.finalize();
+    println!("{:?}", result.into_bytes());
+    let expected = hex!("
+        97d2a569059bbcd8ead4444ff99071f4
+        c01d005bcefe0d3567e1be628e5fdcd9
+    ");
+    println!("{:?}", expected);
 }
